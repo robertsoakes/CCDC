390baf8e712d7031d7a2772ea0d332247fc117a7
Jason Bell <jbell@accelebrate.com>
2019-07-17 19:18:37 -0700

    Code

---
+++ Code/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace EComm.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public string Package { get; set; }
+        public bool IsDiscontinued { get; set; }
+
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+}
---
+++ MultiStage/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (void)
+{
+  printf ("Goodbye, world!\n");
+  return 0;
+}
